67ea7a5a41dd555e3b6d4a364d028f0e9a1f3f5e
Anandsoni992 <anandsoni9165@gmail.com>
2025-01-29 12:22:10 +0530

    queue using stack

---
+++ Day2/src/stackandqueues/queueusingstack/QueueUsingStacks.java
@@ -0,0 +1,35 @@
+package stackandqueues.queueusingstack;
+
+import java.util.Stack;
+
+class QueueUsingStacks {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    // Enqueue operation
+    void enqueue(int x) {
+        stack1.push(x);
+    }
+
+    // Dequeue operation
+    int dequeue() {
+        if (stack2.isEmpty()) {
+            if (stack1.isEmpty()) {
+                throw new IllegalStateException("Queue is empty");
+            }
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingStacks queue = new QueueUsingStacks();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        System.out.println(queue.dequeue()); // 1
+        System.out.println(queue.dequeue()); // 2
+    }
+}
